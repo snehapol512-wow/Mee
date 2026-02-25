42b062dee58321cde0b88ffda835795921a12a80
snehapol512-wow <snehapol512@gmail.com>
2026-02-25 19:14:38 +0530

    added new files

---
+++ src/Animal.java
@@ -0,0 +1,19 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
+
+class InheritanceDemo {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();
+        d.bark();
+    }
+}
---
+++ src/PalindromeCheck.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PalindromeCheck {
+
+    static boolean isPalindrome(String str) {
+        String reversed = new StringBuilder(str).reverse().toString();
+        return str.equalsIgnoreCase(reversed);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        if (isPalindrome(str))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not a Palindrome");
+    }
+}
